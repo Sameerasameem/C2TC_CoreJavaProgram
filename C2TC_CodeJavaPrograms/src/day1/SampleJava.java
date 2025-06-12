3b13edfe9382ac443ea79cf90111dd0bc13ef3d3
Asif0106 <mashifkhan7@gmail.com>
2025-06-12 10:24:02 +0530

    This project contains my core java Practical programs

---
+++ C2TC_CodeJavaPrograms/src/day1/SampleJava.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Java Session..");
+		// TODO Auto-generated method stub
+
+	}
+
+}
